3d0e4911a60476b0371048b0ec4d90524d6ab776
Alexander Bykovsky <alexander.bykovsky@macys.com>
2015-01-21 21:06:34 +0200

    Update Test class

--- src/main/java/com/my/sibyl/itemsets/hbase/test/TestItemSetsDao.java
+++ src/main/java/com/my/sibyl/itemsets/hbase/test/TestItemSetsDao.java
@@ -5,8 +5,11 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.HConnection;
 import org.apache.hadoop.hbase.client.HConnectionManager;
+import org.apache.hadoop.hbase.exceptions.HBaseException;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author abykovsky
@@ -14,20 +17,27 @@ import java.io.IOException;
  */
 public class TestItemSetsDao {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, HBaseException {
         Configuration myConf = HBaseConfiguration.create();
 
         try(HConnection connection = HConnectionManager.createConnection(myConf)) {
             ItemSetsDao itemSetsDao = new ItemSetsDao(connection);
 
             /*itemSetsDao.updateCount("1", 1);
-            itemSetsDao.updateCount("1", "2", 1);
+            itemSetsDao.updateAssocCount("1", "2", 1);
             itemSetsDao.updateCount("2", 1);
-            itemSetsDao.updateCount("2", "1", 1);
+            itemSetsDao.updateAssocCount("2", "1", 1);
             itemSetsDao.updateCount("1-2", 1);*/
 
-            System.out.println("1-2:" + itemSetsDao.getCount("1-2"));
-            System.out.println("1:" + itemSetsDao.getCount("1", "2"));
+
+            Map<String, Integer> assocMap3 = new HashMap<>();
+            assocMap3.put("1", 1);
+            assocMap3.put("2", 2);
+            itemSetsDao.updateCounts("3", 3, assocMap3);
+
+            System.out.println("3:" + itemSetsDao.getCount("3"));
+            System.out.println("3=>1:" + itemSetsDao.getCount("3", "1"));
+            System.out.println("3=>2:" + itemSetsDao.getCount("3", "2"));
         }
     }
 }
